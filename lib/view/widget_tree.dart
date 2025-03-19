bbc9b9f2fc7b72cd17773a63e0ae2d38548d9ce6
ardhikakrsh <ardhikakrishna@gmail.com>
2025-03-19 13:04:02 +0700

    feat: add widget tree

---
+++ lib/view/widget_tree.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:pakeaja/components/my_drawer.dart';
+import 'package:pakeaja/data/notifiers.dart';
+import 'package:pakeaja/view/pages/cart_page.dart';
+import 'package:pakeaja/view/pages/collection_page.dart';
+import 'package:pakeaja/view/pages/home_page.dart';
+import 'package:pakeaja/view/pages/profile_page.dart';
+import 'package:pakeaja/view/widgets/navbar_widget.dart';
+
+List<Widget> pages = [
+  const HomePage(),
+  const CollectionPage(),
+  const ProfilePage(),
+];
+
+class WidgetTree extends StatelessWidget {
+  const WidgetTree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PakeAja'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const CartPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      drawer: const MyDrawer(),
+      body: ValueListenableBuilder(
+        valueListenable: selectedPageNotifier,
+        builder: (context, selectedPage, child) {
+          return pages.elementAt(selectedPage);
+        },
+      ),
+      bottomNavigationBar: const NavbarWidget(),
+    );
+  }
+}
