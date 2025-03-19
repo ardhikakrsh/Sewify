592578dc6995ddc2c9d72ad20d5ff64481611959
ardhikakrsh <ardhikakrishna@gmail.com>
2025-03-20 01:33:14 +0700

    feat: update crud goods

--- lib/view/pages/collection_page.dart
+++ lib/view/pages/collection_page.dart
@@ -1,14 +1,155 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:pakeaja/components/my_list.dart';
+import 'package:pakeaja/service/database/firestore.dart';
 
-class CollectionPage extends StatelessWidget {
+class CollectionPage extends StatefulWidget {
   const CollectionPage({super.key});
 
   @override
+  State<CollectionPage> createState() => _CollectionPageState();
+}
+
+class _CollectionPageState extends State<CollectionPage> {
+  final FirestoreService firestoreService = FirestoreService();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    priceController.dispose();
+    descriptionController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Collection Page'),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: openNoteBox,
+        child: const Icon(Icons.add),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: firestoreService.getGoodsStream(),
+        builder: (context, snapshot) {
+          // if we have data, get all the docs
+          if (snapshot.hasData) {
+            List goodsList = snapshot.data!.docs;
+
+            return ListView.builder(
+              itemCount: goodsList.length,
+              itemBuilder: (context, index) {
+                // get each individual doc
+                DocumentSnapshot document = goodsList[index];
+                String docID = document.id;
+
+                // get the goods from each doc
+                Map<String, dynamic> data =
+                    document.data() as Map<String, dynamic>;
+                String name = data['name'];
+                String price = data['price'];
+                String description = data['description'];
+
+                // display as a list tile
+                return SingleChildScrollView(
+                  child: MyList(
+                    name: name,
+                    price: price,
+                    description: description,
+                    docID: docID,
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Text('Add some goods..');
+          }
+        },
       ),
     );
+  }
+
+  void openNoteBox() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        title: const Text('Add Goods',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: 'Name',
+                prefixIcon: const Icon(Icons.text_fields),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: priceController,
+              decoration: InputDecoration(
+                labelText: 'Price',
+                prefixIcon: const Icon(Icons.attach_money),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: descriptionController,
+              decoration: InputDecoration(
+                labelText: 'Goods Description',
+                prefixIcon: const Icon(Icons.description),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.redAccent,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Cancel'),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blueAccent,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              firestoreService.addGoods(
+                nameController.text,
+                priceController.text,
+                descriptionController.text,
+              );
+
+              nameController.clear();
+              priceController.clear();
+              descriptionController.clear();
+              Navigator.pop(context);
+            },
+            child: const Text('Add'),
+          ),
+        ],
+      ),
+    ); // Add goods to the collection
   }
 }
